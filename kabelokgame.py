06d8ddf1f422d926aefed17230dc3c2e1ebb833e
Kabelo-kk78 <kabelokgame1@gmail.com>
2025-04-22 16:36:36 +0200

    Add files via upload

---
+++ kabelokgame.py
@@ -0,0 +1,8 @@
+name = input ('Enter your name:')
+age= int(input("Enter your age: "))
+next_year_age  = age +1
+print(f'Hello {name} next year your age will be {next_year_age}  years old ' )
+
+
+
+
